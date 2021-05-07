5c556ad1432e1050f39ad39bc5e7b138098792d7
Neil Stewart <madcdevelop@gmail.com>
2021-05-07 05:59:43 -0700

    Added a directional light. Added multiple cubes.

--- bananas-exim/content/test_fs.glsl
+++ bananas-exim/content/test_fs.glsl
@@ -7,7 +7,8 @@ struct Material {
 };
 
 struct Light {
-    vec3 position;
+    // vec3 position;
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -26,12 +27,14 @@ uniform vec3 viewPos;
 
 void main() 
 {
+    vec3 lightDir = normalize(-light.direction);
+
     // Ambient lighting
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, uv));
 
     // Diffuse map/lighting (max() so there are no negative values)
     vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(light.position - norm);
+    // vec3 lightDir = normalize(light.position - norm);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, uv));
 
--- bananas-exim/src/Renderer.cpp
+++ bananas-exim/src/Renderer.cpp
@@ -35,11 +35,18 @@ void Renderer::Draw(float timestep)
     GLCALL(glClearColor(0.1f, 0.1f, 0.1f, 1.0f));
     GLCALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
-    // Move light around scene
-    float lightPosX = 1.5f * sin(timestep);
-    float lightPosY = 1.2f;
-    float lightPosZ = 2.0f * cos(timestep);
-    glm::vec3 lightPos = glm::vec3(lightPosX, lightPosY, lightPosZ);
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     // textured cube
     m_Shader1.UseProgram();
@@ -51,7 +58,7 @@ void Renderer::Draw(float timestep)
     glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f);
     glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
 
-    m_Shader1.SetVec3("light.position", lightPos);
+    m_Shader1.SetVec3("light.direction", -0.2f, -1.0f, -0.3f);
     m_Shader1.SetVec3("light.ambient", ambientColor);
     m_Shader1.SetVec3("light.diffuse", diffuseColor);
     m_Shader1.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
@@ -62,26 +69,34 @@ void Renderer::Draw(float timestep)
     m_Model->m_IndexBuffer.Bind();
     m_Model->m_Textures[0].Bind(0);
     m_Model->m_Textures[1].Bind(1);
-    GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
 
-    // mvp
-    glm::mat4 model = glm::mat4(1.0f);
+    for(unsigned int i = 0; i < 10; i++)
+    {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        float angle = 20.0f * i;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        m_Shader1.SetMatrix4("model", GL_FALSE, model);
+
+        GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
+    }
+    
+    // camera
     glm::mat4 view = glm::lookAt(m_Camera.m_Position, m_Camera.m_Position + m_Camera.m_Front, m_Camera.m_Up);
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)m_Window->m_Width/(float)m_Window->m_Height, 0.1f, 100.0f);
-    m_Shader1.SetMatrix4("model", GL_FALSE, model);
     m_Shader1.SetMatrix4("view", GL_FALSE, view);
     m_Shader1.SetMatrix4("projection", GL_FALSE, projection);
 
     // light cube
-    m_ShaderLight.UseProgram();
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, lightPos);
-    model = glm::scale(model, glm::vec3(0.2f));
-    m_ShaderLight.SetMatrix4("model", GL_FALSE, model);
-    m_ShaderLight.SetMatrix4("view", GL_FALSE, view);
-    m_ShaderLight.SetMatrix4("projection", GL_FALSE, projection);
+    // m_ShaderLight.UseProgram();
+    // model = glm::mat4(1.0f);
+    // model = glm::translate(model, lightPos);
+    // model = glm::scale(model, glm::vec3(0.2f));
+    // m_ShaderLight.SetMatrix4("model", GL_FALSE, model);
+    // m_ShaderLight.SetMatrix4("view", GL_FALSE, view);
+    // m_ShaderLight.SetMatrix4("projection", GL_FALSE, projection);
 
-    GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
+    // GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
 }
 
 }
