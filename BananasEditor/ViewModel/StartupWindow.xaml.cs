62b4baf5546a67683328c71ac60218f6f031e50b
Neil Stewart <madcdevelop@gmail.com>
2021-09-12 15:58:58 -0700

    Created startup flow to create a new scene and open scenes.

---
+++ BananasEditor/ViewModel/StartupWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace BananasEditor
+{
+    /// <summary>
+    /// Interaction logic for StartupWindow.xaml
+    /// </summary>
+    public partial class StartupWindow : Window
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        { 
+            Application.Current.Shutdown();
+        }
+
+        private void buttonNewScene_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void buttonOpenScene_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = openFileDlg.ShowDialog();
+
+            if(result == true)
+            {
+                MessageBox.Show("Scene opened!");
+            }
+        }
+    }
+}
